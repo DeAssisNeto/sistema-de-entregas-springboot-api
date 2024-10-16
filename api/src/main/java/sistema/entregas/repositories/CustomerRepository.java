4797ca5d54e398215eb2f4020e464ca4280e4764
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-10-16 08:30:59 -0300

    feat(service): add CustomerService class.

--- api/src/main/java/sistema/entregas/repositories/CustomerRepository.java
+++ api/src/main/java/sistema/entregas/repositories/CustomerRepository.java
@@ -1,9 +1,11 @@
 package sistema.entregas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sistema.entregas.models.CustomerModel;
 
 import java.util.UUID;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerModel, UUID> {
 }
---
+++ api/src/main/java/sistema/entregas/services/CustomerService.java
@@ -0,0 +1,7 @@
+package sistema.entregas.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+}
