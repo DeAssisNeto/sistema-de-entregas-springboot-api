d6a1db8083b62c766bce67e5e60156a25b9171be
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-10-16 08:17:39 -0300

    feat(model): implement CustomerModel with basic attributes.

---
+++ api/src/main/java/sistema/entregas/models/CustomerModel.java
@@ -0,0 +1,26 @@
+package sistema.entregas.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Table(name = "costumer_tb")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String cpf;
+}
