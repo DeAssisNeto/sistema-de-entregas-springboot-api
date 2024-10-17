1664cc3717ff5c6b1c6af81ef70973330eef12f1
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-10-17 16:34:53 -0300

    feat(service): add getAll method in CustomerService.

--- api/src/main/java/sistema/entregas/services/CustomerService.java
+++ api/src/main/java/sistema/entregas/services/CustomerService.java
@@ -1,7 +1,17 @@
 package sistema.entregas.services;
 
 import org.springframework.stereotype.Service;
+import sistema.entregas.models.CustomerModel;
+import sistema.entregas.repositories.CustomerRepository;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    private CustomerRepository customerRepository;
+
+
+    public List<CustomerModel> getAll() {
+        return customerRepository.findAll();
+    }
 }
