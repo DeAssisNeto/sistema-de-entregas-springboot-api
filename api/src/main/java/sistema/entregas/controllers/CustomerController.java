3f4f7d1319e86f8091a61a62f31eff6f01866522
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-10-17 16:38:25 -0300

    feat(controller): add findAll endpoint in CustomerController.

--- api/src/main/java/sistema/entregas/controllers/CustomerController.java
+++ api/src/main/java/sistema/entregas/controllers/CustomerController.java
@@ -1,10 +1,24 @@
 package sistema.entregas.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sistema.entregas.models.CustomerModel;
+import sistema.entregas.services.CustomerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<CustomerModel>> findAll(){
+        return ResponseEntity.ok(customerService.getAll());
+    }
 }
