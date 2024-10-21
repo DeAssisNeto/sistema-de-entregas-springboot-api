55a75222115af8227f23bb919b4583fc93b7ed67
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-10-21 17:31:38 -0300

    test(main): add test coverage for main method in ApiApplicationTests.

--- api/src/main/java/sistema/entregas/config/LocalConfig.java
+++ api/src/main/java/sistema/entregas/config/LocalConfig.java
@@ -1,17 +1,25 @@
 package sistema.entregas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import sistema.entregas.models.CustomerModel;
+import sistema.entregas.repositories.CustomerRepository;
+
+import java.util.List;
 
 @Configuration
 @Profile(("local"))
 public class LocalConfig {
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Bean
-    public void startDB(){
+    public void startDB(CustomerRepository customerRepository){
         CustomerModel customer1 = new CustomerModel("Klayvert","12345678912");
         CustomerModel customer2 = new CustomerModel("Leonardo", "98765432198");
+
+        customerRepository.saveAll(List.of(customer1, customer2));
     }
 }
